2665ea2dabee1a2beb9c5e3ea4a20b3b64a841c2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-22 14:44:44 -0500

    Marlborough Mall 2.0 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -75,7 +75,9 @@ function init(e){
     $('#option_selector').change(function(){
         window.location = $(this).val();
     });
-    
+    $('.link_selector').change(function(){
+        window.location = $(this).val();
+    });
     //dynamically changing copyright year
     var current_year = moment().year();
     $("#current_year").text(current_year);
